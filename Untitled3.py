8a65159f6b4916beb9964b8ca7ca54d9100e4649
AKRAM-HUSSAIN <110822870+AKRAM-HUSSAIN@users.noreply.github.com>
2022-08-08 17:38:03 +0530

    Add files via upload

---
+++ Untitled3.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+a=5
+print (a)
+
+
+# In[ ]:
+
+
+import pandas as pd
+
+
+# In[9]:
+
+
+pd.read_excel(r"C:\Users\DELL\Documents\Book23.xlsx")
+
+
+# In[11]:
+
+
+pd.read_excel(r"C:\Users\DELL\Documents\Book2.xlsx")
+
+
+# In[12]:
+
+
+pd.read_csv(r"C:\Users\DELL\Documents\Book23.csv")
+
+
+# In[13]:
+
+
+a=5
+b=6
+c=a+b
+print(c)
+
+
+# In[19]:
+
+
+a=10
+print(a)
+
+
+# In[20]:
+
+
+print(type(a))
+
+
+# In[21]:
+
+
+b=10.6
+print(b)
+
+
+# In[22]:
+
+
+print(type(b))
+
+
+# In[29]:
+
+
+a=6
+b=7
+c=a+b
+print(c)
+
+
+# In[30]:
+
+
+print(type(c))
+
+
+# In[33]:
+
+
+pd.read_csv(r"C:\Users\DELL\Documents\Book1.txt")
+
+
+# In[34]:
+
+
+pd.read_csv(r"C:\Users\DELL\Documents\Book1.csv")
+
+
+# In[35]:
+
+
+pd.read_excel(r"C:\Users\DELL\Documents\Book23.xlsx")
+
+
+# In[36]:
+
+
+pd.read_csv(r"C:\Users\DELL\Documents\Book23.txt")
+
+
+# In[37]:
+
+
+import os
+os.getcwd()
+
+
+# In[47]:
+
+
+import pandas as pd
+
+
+# In[59]:
+
+
+pd.read_csv(r"C:\Users\DELL\Book 12.txt")
+
+
+# In[61]:
+
+
+pd.read_csv("Book 12.txt",sep="\t")
+
+
+# In[80]:
+
+
+pd.read_csv("Book 12.txt",sep="\t")
+
+
+# In[ ]:
+
+
+
+
