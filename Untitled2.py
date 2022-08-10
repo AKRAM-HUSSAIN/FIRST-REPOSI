fb1f97229fe8714deb8fdd1ec241862f596ad73e
AKRAM-HUSSAIN <110822870+AKRAM-HUSSAIN@users.noreply.github.com>
2022-08-10 18:00:49 +0530

    Add files via upload

---
+++ Untitled2.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[20]:
+
+
+pd.read_csv("https://raw.githubusercontent.com/dsrscientist/DSData/master/Concrete_Data.csv")
+
+
+# In[21]:
+
+
+pd.read_csv("https://raw.githubusercontent.com/dsrscientist/DSData/master/SBI_Life_insurance.csv")
+
+
+# In[22]:
+
+
+import os
+
+
+# In[23]:
+
+
+os.getcwd()
+
+
+# In[29]:
+
+
+pd.read_csv("Book 12.txt")
+
+
+# In[31]:
+
+
+pd.read_csv("book 13.txt",sep="\t")
+
+
+# In[34]:
+
+
+pd.read_excel(r"C:\Users\DELL\Documents\Book1.xlsx")
+
+
+# In[35]:
+
+
+pd.read_csv(r"C:\Users\DELL\Documents\Book2.csv")
+
+
+# In[37]:
+
+
+pd.read_csv(r"C:\Users\DELL\Documents\Book3.txt")
+
+
+# In[41]:
+
+
+pd.read_csv("book 14.txt",sep="\t")
+
+
+# In[42]:
+
+
+a=[]
+a
+
+
+# In[46]:
+
+
+a=['a',10,15,10.6]
+a
+
+
+# In[48]:
+
+
+type(a)
+
+
+# In[52]:
+
+
+a='vishal'
+type(a)
+
+
+# In[53]:
+
+
+marks=(15,25,46,28,45)
+marks
+
+
+# In[ ]:
+
+
+
+
