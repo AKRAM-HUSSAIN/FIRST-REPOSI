c44b4f03d12e5ab81513c8bab6686fe560e3bd5c
AKRAM-HUSSAIN <110822870+AKRAM-HUSSAIN@users.noreply.github.com>
2022-08-10 16:18:58 +0530

    Add files via upload

---
+++ Untitled2.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[4]:
+
+
+pd.read_excel(r"C:\Users\DELL\Desktop\Book12.xlsx")
+
+
+# In[5]:
+
+
+pd.read_csv(r"C:\Users\DELL\Desktop\Book13.csv")
+
+
+# In[13]:
+
+
+pd.read_csv(r"C:\Users\DELL\Desktop\Book14.txt")
+
+
+# In[14]:
+
+
+import os
+
+
+# In[ ]:
+
+
+
+
